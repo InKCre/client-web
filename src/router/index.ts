87bf0dad23328e91ddd82ec9faf799750f18f9ea
Lanzhijiang <lanzhijiang@foxmail.com>
2025-11-21 18:10:27 +0800

    sync work

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import StartView from '@/views/startView.vue'
-import FocusExplorerView from '@/views/focusExplorerView.vue'
-import ExtensionView from '@/views/extensionView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import StartView from "@/views/startView.vue";
+import FocusExplorerView from "@/views/focusExplorerView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'Home',
+      path: "/",
+      name: "Home",
       component: StartView,
     },
     {
-      path: '/workspace',
-      name: 'Workspace',
-      component: StartView,
-    },
-    {
-      path: '/focus/:blockId',
-      name: 'FocusExplorer',
+      path: "/focus/:blockId",
+      name: "FocusExplorer",
       component: FocusExplorerView,
       props: (route) => ({ blockId: Number(route.params.blockId) }),
     },
-    {
-      path: '/extensions',
-      name: 'Extensions',
-      component: ExtensionView,
-    },
   ],
-})
+});
 
-export default router
+export default router;
